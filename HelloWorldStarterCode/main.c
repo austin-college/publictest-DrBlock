fdb63b4bf2db46e3079f3555574a3f33e71d05ab
Aaron Block <developer@aaronblock.com>
2019-01-24 12:14:43 -0600

    Initial Commit

---
+++ HelloWorldStarterCode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorldStarterCode
+//
+//  Created by Aaron David Block on 1/24/19.
+//  Copyright © 2019 Aaron David Block. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
